0d7891819720da59bf27d761d9c029eb0d282890
Peter Knipp <peteraknipp@gmail.com>
2020-08-10 14:30:04 -0400

    Make a few minor changes before lunch on Monday

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//const apiRouter = require('./routes/api');
+//const pagesRouter= require('./routes/pages');
+
 app.set("view engine", "pug");
 
 app.use(require("morgan")('dev'));
@@ -8,6 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require('cookie-parser')());
 app.use("/public", express.static('public'));
+
+//app.use('/api', apiRouter);
+//app.use('/', pagesRouter);
+
 app.use((req, res, next) => res.render('error-page'));
 
 const csrfProtection = require('csurf')({ cookie: true });

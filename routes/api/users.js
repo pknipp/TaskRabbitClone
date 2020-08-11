cc8d1bbc014b024a18bfbc0d5cce2f7f5a4a6f5b
Simon Carroll <snc40@georgetown.edu>
2020-08-11 13:51:24 -0400

    Preliminary changes to /api/users signup route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,53 @@
 
 const express = require("express");
 const router = express.Router();
+const { routeHandler, handleValidationErrors } = require('../utils');
+const { getUserToken } = require('../utils/auth');
+const bcrypt = require('bcryptjs');
+const { expiresIn } = require('../../config').jwtConfig;
+const db = require('../../db/models');
+const { User } = db;
 
-router.post('/', (req, res, next) => {
+const { check }= require('express-validator');
 
-});
+const validateAuthFields = [
+  check("lastName", "Last name must be between 1 and 80 characters")
+    .exists()
+    .isLength({ min: 1, max: 80 }),
+  check("email", 'Email must be a valid email between 5 and 80 characters.')
+    .exists()
+    .isLength({ min: 5, max: 80 })
+    .isEmail(),
+  check("password", "Password field must be 6 or more characters")
+    .exists()
+    .isLength({ min: 6, max: 70 }),
+  check('password2', 'Confirm password field must have the same value as the password field')
+    .exists()
+    .custom((value, { req }) => value === req.body.password)
+]
+
+router.post(
+  "/signup",
+  csrfProtection,
+  validateAuthFields,
+  handleValidationErrors,
+  routeHandler(async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+      hashedPassword: bcrypt.hashSync(password, 10),
+    });
+
+    const token = await getUserToken(user);
+    res.cookie("token", token, { maxAge: expiresIn * 1000 }); // maxAge in milliseconds
+
+    res.json({ id: user.id, token });
+  })
+);
 
 router.post('/token', (req, res, next) => {
 

1b50ee5fc2744abbff8e73dc926573c836702f24
Nick Mallas <nmallas23@gmail.com>
2020-08-11 16:55:14 -0500

    added event listener and connected to login form

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+
+const form = document.getElementById("login-form");
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const _csrf = formData.get("_csrf");
+    const res = await fetch("/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+    })
+    const data = await res.json();
+    if(!res.ok) {
+        const { message } = data;
+        const errorsContainer = document.getElementById("errors-container");
+        errorsContainer.innerHTML = message;
+        return;
+    }
+    window.location.href = "/"
+
+})

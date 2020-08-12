25f31e7e97b9979c89f5dd3c874d20b7b7867c76
Nick Mallas <nmallas23@gmail.com>
2020-08-12 15:50:58 -0500

    successfully added all taskers to task page

--- public/js/taskers.js
+++ public/js/taskers.js
@@ -1,35 +1,30 @@
 
 const dataDiv = document.getElementById("dataDiv");
 const jobTypeId = dataDiv.dataset.job;
-let taskers;
 const getTaskers = async (jobTypeId) => {
-    const data = await fetch(`/api/jobtypes/${jobTypeId}`);
-    taskers = await data.json();
-    console.log(taskers)
+    const res = await fetch(`/api/jobtypes/${jobTypeId}`);
+    let taskers = await res.json();
+
+    let taskersContainer = document.getElementById("taskersContainer");
+    if(res.ok) {
+        taskers.forEach(tasker => {
+            let taskerContainer = document.createElement("div");
+            taskerContainer.classList.add("tasker");
+            let nameDiv = document.createElement("div");
+            nameDiv.innerHTML = `Name: ${tasker.name}`;
+            let skillDiv = document.createElement("div")
+            skillDiv.innerHTML = `Skill: ${tasker.skill}`;
+            let priceDiv = document.createElement("div")
+            priceDiv.innerHTML = `Price: ${tasker.price}`;
+            taskerContainer.appendChild(nameDiv);
+            taskerContainer.appendChild(skillDiv);
+            taskerContainer.appendChild(priceDiv);
+
+            // add individulal tasker to taskers container
+            taskersContainer.appendChild(taskerContainer);
+        })
+
+    }
 }
 
-
-getTaskers(jobTypeId);
-
-console.log(taskers);
-
-let taskersContainer = document.getElementById("taskersContainer");
-if(taskers) {
-    taskers.forEach(tasker => {
-        let taskerContainer = document.createElement("div");
-        taskerContainer.classList.add("tasker");
-        let nameDiv = document.createElement("div");
-        nameDiv.innerHTML = tasker.name;
-        let skillDiv = document.createElement("div")
-        skillDiv.innerHTML = tasker.skill;
-        let priceDiv = document.createElement("div")
-        priceDiv.innerHTML = tasker.skill;
-        taskerContainer.appendChild(nameDiv);
-        taskerContainer.appendChild(skillDiv);
-        taskerContainer.appendChild(priceDiv);
-
-        // add individulal tasker to taskers container
-        taskersContainer.appendChild(taskerContainer);
-
-    })
-}
+getTaskers(jobTypeId)

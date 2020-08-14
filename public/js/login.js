817e53daf19bbf182272102ef19369a6b1270809
Nick Mallas <nmallas23@gmail.com>
2020-08-13 20:57:54 -0500

    finished auth and added demo user

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,29 @@
 
 const form = document.getElementById("login-form");
+const demoUser = document.getElementById("demo");
+
+demoUser.addEventListener("click", async e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const _csrf = formData.get("_csrf");
+    const res = await fetch("/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email:"demo@user.io", password:"password", _csrf })
+    })
+    const data = await res.json();
+    if(!res.ok) {
+        const { message } = data;
+        const errorsContainer = document.getElementById("errors-container");
+        errorsContainer.innerHTML = message;
+        errorsContainer.style.display = "block";
+        return;
+    }
+    window.location.href = "/home"
+})
+
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(form);

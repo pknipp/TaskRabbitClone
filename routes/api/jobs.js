9d8253d19053b9d7cc4949e3eb8f4d285cfa2da3
Nick Mallas <nmallas23@gmail.com>
2020-11-09 13:12:58 -0600

    change default job sorting to sort by date

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -5,12 +5,13 @@ const { routeHandler, handleValidationErrors } = require('../utils');
 const { User, Job, Tasker, JobType } = require("../../db/models");
 
  router.get("/:id(\\d+)/", routeHandler( async(req, res) => {
-     const jobs = await Job.findAll({where: {userId: req.params.id}, include: [User, Tasker]});
+     const jobs = await Job.findAll({where: {userId: req.params.id}, include: [User, Tasker], order: ["jobDate"]});
      res.json(jobs.map(job => job.dataValues));
  }))
 
 router.get("/:id(\\d+)/:sort(\\d+)", routeHandler( async(req, res) => {
-     let options = [["userId"],["jobDate"],[[Tasker, "name"]],[[Tasker,"price"]]];
+    // set default sorting to be date
+    let options = [["jobDate"],["jobDate"],[[Tasker, "name"]],[[Tasker,"price"]]];
     const jobs = await Job.findAll({where: {userId: req.params.id}, include: [User, Tasker], order: options[req.params.sort]});
      // continue trying to refactor code as follows, so that jobType.name can be included on page
     //  const jobs = await Tasker.findAll({include: [User, Job, JobType]});

ebbe4b31b5615f1097151a7b84457ab8399c6677
Peter Knipp <peteraknipp@gmail.com>
2021-02-05 10:54:33 -0500

    Stop ignoring .env

--- config/index.js
+++ config/index.js
@@ -2,10 +2,10 @@ module.exports = {
   environment: process.env.NODE_ENV || "development",
   port: process.env.PORT || 4000,
   db: {
-    username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || 'postgres',
   },
   jwtConfig: {
     secret: process.env.JWT_SECRET,
